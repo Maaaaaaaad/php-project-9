8634e489b3114e9305575271d6adfddc62082c67
Maaaaaaaad <wowa-iff@yandex.ru>
2024-09-10 14:03:41 +0300

    Add database.sql

--- public/index.php
+++ public/index.php
@@ -7,6 +7,14 @@ use Slim\Factory\AppFactory;
 use Slim\Middleware\MethodOverrideMiddleware;
 
 
+
+/*$databaseUrl = parse_url($_ENV['DATABASE_URL']);
+$username = $databaseUrl['user']; // janedoe
+$password = $databaseUrl['pass']; // mypassword
+$host = $databaseUrl['host']; // localhost
+$port = $databaseUrl['port']; // 5432
+$dbName = ltrim($databaseUrl['path'], '/'); // mydb*/
+
 $container = new Container();
 $container->set('renderer', function () {
     return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
@@ -22,54 +30,6 @@ $app->get('/', function ($request, $response) {
     return $this->get('renderer')->render($response, 'index.phtml');
 });
 
-/*
-
-$app->post('/cart-items', function ($request, $response) {
-
-
-    $item[] = $request->getParsedBodyParam('item');
-    $cart = json_decode($request->getCookieParam('cart', json_encode([])), true);
-
-    foreach ($item as $array) {
-        foreach ($array as $key => $value) {
-            if ($key == 'id' && $value == 1) {
-                $cart["$value"]['name'] = 'One';
-                $cart["$value"]['count'] += 1;
-            } elseif ($key == 'id' && $value == 2) {
-                $cart["$value"]['name'] = 'Two';
-                $cart["$value"]['count'] +=2;
-            }
-        }
-    }
-
-    foreach ($item as $array) {
-         foreach ($array as $key => $value) {
-             if ($key == 'id' && $value == 1) {
-                 $count1 += 1;
-             } elseif ($key == 'id' && $value == 2) {
-                 $count2 += 2;
-             }
-         }
-     }
-
-         $cart = [
-             ['name' => 'One', 'count' => $count1],
-             ['name' => 'Two', 'count' => $count2]
-         ];
-
-    $encodedCart = json_encode($cart);
-
-    return $response->withHeader('Set-Cookie', "cart={$encodedCart}")->withRedirect('/');
-});
-
-
-$app->delete('/cart-items', function ($request, $response) {
-
-    $encodedCart = json_encode($cart = []);
-
-    return $response->withHeader('Set-Cookie', "cart={$encodedCart}")
-        ->withRedirect('/');
-});*/
 
 
 $app->run();
