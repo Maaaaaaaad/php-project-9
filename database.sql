42570d87d2c1f2016f78eb0e5f2e444a229d6a10
Maaaaaaaad <wowa-iff@yandex.ru>
2024-09-14 23:37:40 +0300

    fix database.sql

---
+++ database.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS urls (
+                                    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+                                    name varchar(255),
+                                    created_at TIMESTAMP
+);
+CREATE TABLE IF NOT EXISTS url_checks (
+                                          id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+                                          url_id BIGINT REFERENCES urls(id) NOT NULL,
+                                          status_code int,
+                                          h1 varchar(255),
+                                          title varchar(255),
+                                          description varchar(255),
+                                          created_at TIMESTAMP
+);
