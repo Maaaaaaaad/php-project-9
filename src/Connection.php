253d8c8aaa4656d81fa6ab94e22f8e2f092f21c7
Maaaaaaaad <wowa-iff@yandex.ru>
2024-09-14 12:48:30 +0300

    update gitignore

--- src/Connection.php
+++ src/Connection.php
@@ -32,22 +32,6 @@ final class Connection
             $username,
             $password
         );
-
-/*        $params = parse_ini_file('database.ini');
-        if ($params === false) {
-            throw new \Exception("Error reading database configuration file");
-        }
-
-        $conStr = sprintf(
-            "pgsql:host=%s;port=%d;dbname=%s;user=%s;password=%s",
-            $params['host'],
-            $params['port'],
-            $params['database'],
-            $params['user'],
-            $params['password']
-        );*/
-
-
         $pdo = new \PDO($conStr);
         $pdo->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
 
